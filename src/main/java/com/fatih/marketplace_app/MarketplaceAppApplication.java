545d43b0b5844cdcef6aa3fb7446a18f77f01bba
fatihhozkurt <fatihhhozkurt@gmail.com>
2025-02-22 02:57:19 +0300

    initial commit

---
+++ src/main/java/com/fatih/marketplace_app/MarketplaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.fatih.marketplace_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatih/marketplace_app/MarketplaceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatih.marketplace_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
